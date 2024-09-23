f79212c7db5f2dd0e95656dcaebd1c7ea48423d3
Kairat Ubukulov <ubukulov@gmail.com>
2024-09-23 18:00:23 +0500

    fx

--- app/Services/FirebaseService.php
+++ app/Services/FirebaseService.php
@@ -33,7 +33,29 @@ class FirebaseService
             ],
         ];
 
-        $accessToken = $this->googleClient->fetchAccessTokenWithAssertion()['access_token'];
+        // Получение access token с помощью Google API Client
+        $accessToken = $this->googleClient->fetchAccessTokenWithAssertion();
+
+        if (isset($accessToken['error'])) {
+            throw new \Exception('Ошибка при получении access token: ' . $accessToken['error_description']);
+        }
+
+        try {
+            // Отправка HTTP POST запроса на FCM API
+            $response = $this->httpClient->post($url, [
+                'headers' => [
+                    'Authorization' => 'Bearer ' . $accessToken['access_token'],
+                    'Content-Type' => 'application/json',
+                ],
+                'json' => $message, // JSON-кодирование сообщения
+            ]);
+
+            return $response->getBody()->getContents();
+        } catch (\Exception $e) {
+            throw new \Exception('Ошибка при отправке уведомления: ' . $e->getMessage());
+        }
+
+        /*$accessToken = $this->googleClient->fetchAccessTokenWithAssertion()['access_token'];
 
         $response = $this->httpClient->post($url, [
             'headers' => [
@@ -43,6 +65,6 @@ class FirebaseService
             'json' => $message,
         ]);
 
-        return $response->getBody()->getContents();
+        return $response->getBody()->getContents();*/
     }
 }
